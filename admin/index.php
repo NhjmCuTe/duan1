6125d6e8650fbe3a3cd3f4b32108567e3c1ef29c
NhjmCuTe <levantuanquangloi@gmail.com>
2023-11-19 16:23:02 +0700

    sửa admin

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,8 @@ include "../model/pdo.php";
 include "../global.php";
 include "../model/san_pham.php";
 include "../model/danh_muc.php";
+include "../model/kich_thuoc.php";
+
 
 include "header.php";
 
@@ -20,6 +22,17 @@ if (isset($_GET['act']) && $_GET['act'] != '') {
             include "san_pham/ds_san_pham.php";
             break;
         case 'chi_tiet_sp':
+            $all_size = all_size();
+            if (isset($_POST['them_size']) && $_POST['them_size']) {
+                $id_sanpham = $_GET['id_sp'];
+                $id_kichthuoc = $_POST['kich_thuoc'];
+                them_kich_thuoc_sp($id_sanpham,$id_kichthuoc);
+            }
+            if(isset($_GET['id_kichthuoc'])&&$_GET['id_kichthuoc']!=''){
+                $id_kichthuoc = $_GET['id_kichthuoc'];
+                $id_sanpham = $_GET['id_sp'];
+                xoa_kichthuoc_sp($id_kichthuoc,$id_sanpham);
+            }
             if (isset($_GET['id_sp']) && $_GET['id_sp'] != '') {
                 $size_sp = size_san_pham($_GET['id_sp']);
                 $mau_sp = mau_san_pham($_GET['id_sp']);
@@ -27,9 +40,14 @@ if (isset($_GET['act']) && $_GET['act'] != '') {
             }
             break;
         case 'anh_theo_mau':
-            if(isset($_GET['id_mau'])&&isset($_GET['them_anh'])&&isset($_POST['them_anh'])){
+            if (isset($_GET['id_mau']) && isset($_GET['them_anh']) && isset($_POST['them_anh'])) {
                 $id_mau = $_GET['id_mau'];
-                
+                $ten_anh = $_FILES['img']['name'];
+                if (move_uploaded_file($_FILES['img']['tmp_name'], '../' . $duong_dan_anh . $ten_anh));
+                them_anh_sp($id_mau, $ten_anh);
+            }
+            if (isset($_GET['id_mau']) && isset($_GET['id_anh_xoa']) && $_GET['id_anh_xoa'] != '') {
+                xoa_anh_sp($_GET['id_anh_xoa']);
             }
             if (isset($_GET['id_mau']) && $_GET['id_mau'] != '') {
                 $anh_theo_mau = anh_theo_mau($_GET['id_mau']);
@@ -73,8 +91,15 @@ if (isset($_GET['act']) && $_GET['act'] != '') {
             }
             include "san_pham/them_kich_thuoc.php";
             break;
-        case 'them_anh_sp':
-
+        case 'ds_kichthuoc':
+            if(isset($_POST['them_kich_thuoc'])&&$_POST['them_kich_thuoc']){
+                them_kich_thuoc($_POST['kich_thuoc']);
+            }
+            if(isset($_GET['id_kichthuoc_xoa'])&&$_GET['id_kichthuoc_xoa']!=''){
+                xoa_kichthuoc($_GET['id_kichthuoc_xoa']);
+            }
+            $all_kich_thuoc = all_kich_thuoc();
+            include "kich_thuoc/ds_kich_thuoc.php";
             break;
         default:
             include "main.php";
