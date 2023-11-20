bf1872dfa9232527f9639fbc36df4d7c33a2b93f
NhjmCuTe <levantuanquangloi@gmail.com>
2023-11-21 02:52:37 +0700

    edit

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+include "model/pdo.php";
+include "model/san_pham.php";
+include "model/danh_muc.php";
+include "model/kich_thuoc.php";
+
+if(isset($_GET['act'])&&$_GET['act'])
+
 include "view/menu.php";
 // include "view/banner.php";
 // include "view/main.php";
